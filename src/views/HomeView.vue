f267f2ff5b4d8f7341fe32376f750d4c18d46691
sskiragu <skiragu18@gmail.com>
2023-08-19 10:44:39 +0300

    Auth - Prof

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,59 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="counter">
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </div>
+    <h1>{{ `Count: ${ count }` }}</h1>
   </div>
 </template>
 
+<script setup>
+/**
+ * Composition API
+ */
+import { ref } from 'vue'
+
+  const count = ref(0)
+
+  const increment = () => count.value++
+  const decrement = () => count.value--
+
+</script>
+
+<!--
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+/*
+Options API
+*/
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      count: 0
+    }
+  },
+  methods: {
+    increment(){
+      this.count++
+    },
+    decrement(){
+      this.count--
+    }
   }
 }
+
 </script>
+-->
+
+<style scoped>
+.counter button{
+  background-color: blue;
+  margin: 14px;
+  padding: 10px;
+  color: white;
+  font-size: 28px;
+  border-style: none;
+}
+</style>>
+
---
+++ src/views/SignupView.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-card
+    class="mx-auto"
+        max-width="500"
+        elevation="0">
+        <v-form v-on:submit.prevent="signup">
+            <v-text-field type="text" aria-label="Username" variant="outlined" v-model="username"></v-text-field>
+            <v-text-field type="email" aria-label="Email" variant="outlined" v-model="email"></v-text-field>
+            <v-text-field type="password" aria-label="Password" variant="outlined" v-model="password"></v-text-field>
+            <v-btn type="submit" variant="outlined">Signup</v-btn>
+        </v-form>
+        {{ username }}
+    </v-card>
+</template>
+
+<script setup>
+/**
+ * Composition API
+ */
+import { ref } from 'vue';
+import axios from 'axios'
+
+const username = ref('')
+const email = ref('')
+const password = ref('')
+
+const signup = async () => {
+const response = await axios.post('http://127.0.0.1:8000/api/signup',{
+    username: username.value,
+    email: email.value,
+    password: password.value
+})
+console.log(response);
+}
+
+
+</script>
